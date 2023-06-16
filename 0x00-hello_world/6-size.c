f7be0b022bb52ccc7d20920423b94a019e19ffe8
Teaeli <132862851+amenyaeli@users.noreply.github.com>
2023-06-16 08:11:18 +0000

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,8 +4,7 @@
 *
 * Return: Always 0 (Hurray)
 */
-int main(void)
-{printf("Size of a char: %d byte(s)\n", sizeof(char));
+int main(void){printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
