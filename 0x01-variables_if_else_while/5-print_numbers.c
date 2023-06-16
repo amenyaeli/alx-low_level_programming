9ad120f3636d1c0e9210158846807e2ddbaeec73
amenyaeli <amenyaeli@gmail.com>
2023-06-16 16:53:09 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry data
+ *
+ * Return: 0 on completion
+ */
+
+int main(void)
+{
+	int nab;
+
+	while (nab < 10)
+	{
+		printf("%d", nab);
+		nab += 1;
+	}
+	printf("\n");
+	return (0);
+}
