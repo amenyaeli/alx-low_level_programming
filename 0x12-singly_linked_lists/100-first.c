d166cb029b936eb2faf9ec71ced3196040272f34
amenyaeli <amenyaeli@gmail.com>
2023-07-27 13:59:11 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
-/**
- * myTortise - Apply the constructor attribute to myTorHar() so that it
- *	is executed before main()
- */
-
-void myTortise(void) __attribute__ ((constructor));
+void first(void) __attribute__ ((constructor));
 
 /**
- * myTortise - Implementation of myTorHar
+ * first - Print sentences before the main function
+ * Return: Sentenses
  */
 
-void myTortise(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
