a4e3079befce9ec3474a5f58bb4bff2c5d0e64fa
amenyaeli <amenyaeli@gmail.com>
2023-08-07 18:34:14 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,19 +13,19 @@ int append_text_to_file(const char *filename, char *text_content)
 	int f, writes, l;
 
 	if (filename == NULL)
-		return (-1);
+	return (-1);
 
 	if (text_content != NULL)
 	{
-		for (l = 0; text_content[l];)
-			l++;
+	for (l = 0; text_content[l];)
+	l++;
 	}
 
 	f = open(filename, O_WRONLY | O_APPEND);
 	writes = write(f, text_content, l);
 
 	if (f == -1 || writes == -1)
-		return (-1);
+	return (-1);
 
 	close(f);
 
